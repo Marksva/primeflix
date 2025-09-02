b8d86a8bc48e637d46baceb06ae4ff78bc415332
Mark Anthony <mark.sva@hotmail.com>
2025-09-02 11:42:16 -0300

    Implement movie fetching in Home component and create API service

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,28 @@
+import { use, useEffect, useState } from 'react'
+import api from '../../services/api'
+
 function Home() {
+
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+        async function loadMovies() {
+            const response = await api.get("movie/now_playing",{
+                params: {
+                    language: 'pt-BR',
+                    page: 1
+                }
+            })
+
+            console.log(response.data.results);
+        }
+
+        loadMovies();
+    }, [])
+
+
+
+
     return (
         <div>
             <h1>Bem vindo a Home</h1>
