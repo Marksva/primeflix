64dc8db36f176ba64412deeaa6252ab52a3c7f0d
Mark Anthony <mark.sva@hotmail.com>
2025-09-02 20:12:37 -0300

    Implement movie details fetching and add styling for Movie component

--- src/pages/Movie/index.js
+++ src/pages/Movie/index.js
@@ -1,8 +1,59 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from "../../services/api";
+import './movie-info.css';
+
 function Movie() {
+    const { id } = useParams();
+    const [movie, setMovie] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function LoadMovie() {
+            await api.get(`/movie/${id}`, {
+                params: {
+                    language: "pt-BR"
+                }
+            }).then((response) => {
+                // console.log(response.data);
+                setMovie(response.data);
+                setLoading(false);
+            }).catch(() => {
+                console.log("Filme não encontrado");
+            })
+        }
+
+        LoadMovie();
+
+        return () => {
+            console.log("Componente desmontado");
+        }
+
+    }, [])
+
+    if (loading) {
+        return (
+            <div className="movie-info">
+                <h1>Carregando detalhes...</h1>
+            </div>
+        )
+    }
+
     return (
-        <div>
-            <h1>Bem vindo a pagina detalhes do filme</h1>
-        </div>  
+        <div className="movie-info">
+            <h1>{movie.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt={movie.title} />
+            <h3>Sinopse</h3>
+            <span>{movie.overview}</span>
+            <strong>Avaliação: {movie.vote_average} / 10</strong>
+
+            <div className="area-buttons">
+                <button>Salvar</button>
+                <button>
+                    <a href="#">Trailer</a>
+                </button>
+            </div>
+        </div >
     )
 }
 
