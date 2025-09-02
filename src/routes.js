3d3338fc3bd03f0c50c1187ffbcdf1d5a4709295
Mark Anthony <mark.sva@hotmail.com>
2025-09-02 19:35:24 -0300

    Add Erro page with 404 styling and routing

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Movie from "./pages/Movie";
 import Header from "./components/Header";
+import Erro from "./pages/Erro";
 
 function RoutesApp() {
     return (
         <BrowserRouter>
             <Header />
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={< Home />} />
                 <Route path="/movie/:id" element={<Movie />} />
+                
+                <Route path="*" element={<Erro />} />
             </Routes>
         </BrowserRouter>
 
