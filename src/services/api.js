81e76ef1d05290b558a1a2584fa75e8e1b31f670
Mark Anthony <mark.sva@hotmail.com>
2025-09-03 11:16:37 -0300

    Update header styles and enhance API configuration

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://api.themoviedb.org/3/'
+    baseURL: 'https://api.themoviedb.org/3/',
+    headers: {
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MzQzY2Y5MDU2YmJiNDUxNGRjNjI3NTU5YTRmZTA2ZiIsIm5iZiI6MTc1Njc2ODcwOC42MjYsInN1YiI6IjY4YjYyOWM0ZWNjNzJmNjg5ZDNjZDg0NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zjjdcvRlIS6GrOTJRAZRhI03W62Z6KYCgouQuVSbgVo`
+    }
 });
 
 export default api;
