40bcdfce6aed00a3e3674e212c2ff4b2418c5553
Mark Anthony <mark.sva@hotmail.com>
2025-09-03 11:17:15 -0300

    Remove unnecessary headers from API configuration

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://api.themoviedb.org/3/',
-    headers: {
-        Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MzQzY2Y5MDU2YmJiNDUxNGRjNjI3NTU5YTRmZTA2ZiIsIm5iZiI6MTc1Njc2ODcwOC42MjYsInN1YiI6IjY4YjYyOWM0ZWNjNzJmNjg5ZDNjZDg0NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zjjdcvRlIS6GrOTJRAZRhI03W62Z6KYCgouQuVSbgVo`
-    }
+    baseURL: 'https://api.themoviedb.org/3/'
 });
 
 export default api;
