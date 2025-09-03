f77b89f47737239ab2d297166b65d894a5b95a1d
Mark Anthony <mark.sva@hotmail.com>
2025-09-03 15:20:12 -0300

    Refactor Favorites page layout and styles for improved UI consistency

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import './favorites.css';
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
@@ -10,38 +10,33 @@ function Favorites() {
         document.title = "Primeflix - Meus Filmes";
         const myList = localStorage.getItem("@primeflix");
         setMovies(JSON.parse(myList) || []);
-
     }, [])
 
-
     function handleDelete(id) {
-        let filterMovies = movies.filter((item) => {
-            return (item.id !== id)
-        })
-
+        let filterMovies = movies.filter((item) => item.id !== id);
         setMovies(filterMovies);
         localStorage.setItem("@primeflix", JSON.stringify(filterMovies));
         toast.success("Filme removido com sucesso!");
     }
-    
-    return (
-        <div className="my-movies">
-            <h1>Meus Filmes</h1>
-            {movies.length === 0 && <span>Você não possui nenhum filme salvo :(</span>}
-            <ul>
-                {movies.map((movie) => {
-                    return (
-                        <li key={movie.id}>
-                            <span>{movie.title}</span>
-                            <div>
-                                <Link to={`/movie/${movie.id}`}>Ver detalhes</Link>
-                                <button onClick={()=> handleDelete(movie.id)}>Excluir</button>
-                            </div>
-                        </li>
-                    )
-                })}
-            </ul>
 
+    return (
+        <div className="container">
+            <h1>Meus Filmes</h1>
+            {movies.length === 0 && <span className="empty">Você não possui nenhum filme salvo :(</span>}
+            <div className="list-movies">
+                {movies.map((movie) => (
+                    <article key={movie.id}>
+                        <strong>{movie.title}</strong>
+                        <Link to={`/movie/${movie.id}`} className="image-wrapper">
+                            <img src={movie.poster_path ? `https://image.tmdb.org/t/p/original/${movie.poster_path}` : 'https://via.placeholder.com/300x450?text=Sem+Imagem'} alt={movie.title} />
+                        </Link>
+                        <div className="actions">
+                            <Link to={`/movie/${movie.id}`} className='details'>Ver detalhes</Link>
+                            <button className="delete" onClick={() => handleDelete(movie.id)}>Excluir</button>
+                        </div>
+                    </article>
+                ))}
+            </div>
         </div>
     )
 }
