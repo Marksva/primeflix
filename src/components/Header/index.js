a33db95557183d117561b4b42c4be8be2497e600
Mark Anthony <mark.sva@hotmail.com>
2025-09-11 10:47:17 -0300

    check login in header and implementation logout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,32 @@
 import './header.css';
 import { Link } from 'react-router-dom';
-
+import { useAuth } from "../../hooks/useAuth.js";
+import { auth } from '../../services/firebaseConnection';
+import { signOut } from "firebase/auth";
 function Header() {
+    const { user, userDetail } = useAuth();
+
+    async function handleLogout() {
+        await signOut(auth);
+    }
+    
     return (
         <header>
             <Link className='logo text-primary' to="/">PrimeFlix</Link>
             <div className='area-buttons-header'>
                 <Link className='movies btn btn-secondary bold' to="/favorites">Meus filmes 🍿</Link>
-                <Link className='bold btn btn-primary text-light' to="/auth">Entrar</Link>
+                {user ? (
+                    <button
+                        className="bold btn btn-danger text-light"
+                        onClick={handleLogout}
+                    >
+                        Sair
+                    </button>
+                ) : (
+                    <Link className="bold btn btn-primary text-light" to="/auth">
+                        Entrar
+                    </Link>
+                )}
             </div>
         </header>
     )
